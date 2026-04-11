1da65bba3ea8ee3baed9cb4d5b3975fae96a1f38
Daniel Naab <danielnaab@gmail.com>
2026-04-11 14:55:43 +0000

    fix(flex-form): render as div when used as layout wrapper
    Form renders <form> when action/method props are set, <div> when used
    purely for width constraint. Fixes nested <form> elements which caused
    browsers to ignore inner form submissions.

--- src/app/components/flex-form/index.tsx
+++ src/app/components/flex-form/index.tsx
@@ -8,9 +8,16 @@ interface FormProps {
 }
 
 export const Form: FC<FormProps> = ({ action, method, size, children }) => {
+  if (action || method) {
+    return (
+      <form class="flex-form" action={action} method={method} data-size={size}>
+        {children}
+      </form>
+    )
+  }
   return (
-    <form class="flex-form" action={action} method={method} data-size={size}>
+    <div class="flex-form" data-size={size}>
       {children}
-    </form>
+    </div>
   )
 }
