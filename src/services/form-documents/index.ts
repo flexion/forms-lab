3c835ae106fcf211cdf73b4de9d9770125faa8a4
Daniel Naab <danielnaab@gmail.com>
2026-04-18 20:40:46 +0000

    feat(form-documents): add public API index module

---
+++ src/services/form-documents/index.ts
@@ -0,0 +1,14 @@
+export { enumerateFields } from './field-mapping'
+export {
+  createBedrockPdfExtractor,
+  createCachedPdfExtractor,
+} from './extraction'
+export type { BedrockExtractorOptions, PdfExtractor } from './extraction'
+export { fillPdf } from './filling'
+export type {
+  ExtractionOptions,
+  ExtractionResult,
+  FieldConfidence,
+  FieldMapping,
+  FillResult,
+} from './types'
