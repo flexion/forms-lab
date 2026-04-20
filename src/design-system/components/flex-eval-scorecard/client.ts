ec082781f536d2df4d0f9d8af58a97f58b8b2752
Daniel Naab <danielnaab@gmail.com>
2026-04-20 06:45:12 +0000

    chore: remove dead components (authoring-components, flex-criteria-editor, flex-eval-scorecard)

--- src/design-system/components/flex-eval-scorecard/client.ts
+++
@@ -1,9 +0,0 @@
-export class FlexEvalScorecard extends HTMLElement {
-  connectedCallback() {
-    // Read-only server-rendered; no interactive behavior needed yet.
-  }
-}
-
-if (!customElements.get('flex-eval-scorecard')) {
-  customElements.define('flex-eval-scorecard', FlexEvalScorecard)
-}
